fbc9dd50f07463bbb795435d50037c4cea9b883a
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-10-04 23:11:59 +0530

    Filmpire development 1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Filmpire - Saurav
+
+## Steps to Creat the App from Starting
+
+1 - Setup React App
+2 - Installed Dependencies - (Mui, Alan AI, Redux-toolkit, react-redux, react-router-dom and axios)
+3 - setting up App,.js structure
+4- setup React router and folder structure
+
+## Learned New Things
+
+# React Router
+
+# mui
+
+- CssBaseline - Kind of Css reset - Self closing component
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 
+import { Movies, MovieInformation, NavBar, Actors, Profile } from './';
 const App = () => {
   return (
     <div>
-      <h1>This is App Component.</h1>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<h1>This is Home</h1>} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          {/* <Route path="/" element={<h1>This is Home</h1>} /> */}
+        </Routes>
+      </main>
     </div>
   );
 };
